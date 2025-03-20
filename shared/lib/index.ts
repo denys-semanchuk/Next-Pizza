261315b29143afef548ee4331301931216776a87
Denys Semanchuk <semanchukdenys@icloud.com>
2025-03-20 22:08:55 +0100

    realized updateCartTotalAmount function

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -1,4 +1,5 @@
 export { getPizzaDetails } from "./get-pizza-details";
+export { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
 export { getAvailablePizzaSizes } from "./get-available-pizza-sizes";
 export { getCartDetails } from "./get-cart-details";
 export { getCartItemDetails } from "./get-cart-items-details";
