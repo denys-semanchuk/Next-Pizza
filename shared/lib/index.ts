31b678c84a791526fb0217290d066c5f9ab41566
denys-semanchuk <semanchukdenys@icloud.com>
2025-03-22 22:38:38 +0100

    added find or create cart function

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -3,3 +3,4 @@ export { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
 export { getAvailablePizzaSizes } from "./get-available-pizza-sizes";
 export { getCartDetails } from "./get-cart-details";
 export { getCartItemDetails } from "./get-cart-items-details";
+export { findOrCreateCart } from "./find-or-create-cart";
