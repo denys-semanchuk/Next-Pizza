e704dfec8ddbee1b82dde6346cbc9d1eeaf49c20
denys-semanchuk <semanchukdenys@icloud.com>
2025-03-23 15:51:23 +0100

    added Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import { Nunito } from 'next/font/google';
-import './globals.css'
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
 const nunito = Nunito({
-  subsets: ['latin'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 export default function RootLayout({
   children,
@@ -11,12 +12,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <link data-rh="true" rel='icon' href='/logo.png' />
+        <link data-rh="true" rel="icon" href="/logo.png" />
       </head>
       <body className={nunito.className}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
