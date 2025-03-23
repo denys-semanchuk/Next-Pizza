2c08135cbe73720727e994bfbbbe6a10f2f847a8
denys-semanchuk <semanchukdenys@icloud.com>
2025-03-23 15:54:26 +0100

    added hasSearch and hasCart flags

--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -1,45 +1,54 @@
-'use client';
+"use client";
 
-import { cn } from '@/shared/lib/utils';
-import React from 'react';
-import { Container } from './container';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '../ui/button';
-import { User } from 'lucide-react';
-import { SearchInput } from './search-input';
-import { CartButton } from './cart-button';
+import { cn } from "@/shared/lib/utils";
+import React from "react";
+import { Container } from "./container";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { User } from "lucide-react";
+import { SearchInput } from "./search-input";
+import { CartButton } from "./cart-button";
 
 interface Props {
   className?: string;
+  hasSearch?: boolean;
+  hasCart?: boolean;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
-
+export const Header: React.FC<Props> = ({
+  className,
+  hasSearch = true,
+  hasCart = true,
+}) => {
   return (
-    <header className={cn('border-b', className)}>
+    <header className={cn("border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        {/* Левая часть */}
+        {/* Left Part */}
         <Link href="/">
           <div className="flex items-center gap-4">
             <Image src="/logo.png" alt="Logo" width={35} height={35} />
             <div>
               <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-              <p className="text-sm text-gray-400 leading-3">it can&apos;t get any tastier</p>
+              <p className="text-sm text-gray-400 leading-3">
+                it can&apos;t get any tastier
+              </p>
             </div>
           </div>
         </Link>
 
-        <div className='mx-10 flex-1'>
-          <SearchInput/>
-        </div>
+        {hasSearch && (
+          <div className="mx-10 flex-1">
+            <SearchInput />
+          </div>
+        )}
 
-        {/* Правая часть */}
+        {/* Right Part */}
         <div className="flex items-center gap-3">
           <Button variant="outline" className="flex items-center gap-2">
             <User size={16} /> Sign in
           </Button>
-          <CartButton />
+          {hasCart && <CartButton />}
         </div>
       </Container>
     </header>
