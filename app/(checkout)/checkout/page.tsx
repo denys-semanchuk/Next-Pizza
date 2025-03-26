f0990b04a84ecd9bf2c5559b24bebc9c6a69db0e
denys-semanchuk <semanchukdenys@icloud.com>
2025-03-26 21:47:34 +0100

    added checkout page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,7 +1,104 @@
-export default  async function CheckoutPage() {
-    return (
-        <div>
-            <h1>Checkout</h1>
-        </div>
-    )
+'use client';
+
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { CheckoutFormValues, checkoutFormSchema } from '@/shared/constants';
+import { useCart } from '@/shared/hooks';
+import { createOrder } from '@/app/actions';
+import toast from 'react-hot-toast';
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import { Api } from '@/shared/services/api-client';
+import { CheckoutAddressForm, CheckoutCart, CheckoutPersonalForm, CheckoutSidebar, Container, Title } from '@/shared/components/shared';
+
+export default function CheckoutPage() {
+  const [submitting, setSubmitting] = React.useState(false);
+  const { totalAmount, updateItemQuantity, cartItems, removeCartItem, loading } = useCart();
+  const { data: session } = useSession();
+
+  const form = useForm<CheckoutFormValues>({
+    resolver: zodResolver(checkoutFormSchema),
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      comment: '',
+    },
+  });
+
+  React.useEffect(() => {
+    async function fetchUserInfo() {
+      const data = await Api.auth.getMe();
+      const [firstName, lastName] = data.fullName.split(' ');
+
+      form.setValue('firstName', firstName);
+      form.setValue('lastName', lastName);
+      form.setValue('email', data.email);
+    }
+
+    if (session) {
+      fetchUserInfo();
+    }
+  }, [session]);
+
+  const onSubmit = async (data: CheckoutFormValues) => {
+    try {
+      setSubmitting(true);
+
+      const url = await createOrder(data);
+
+      toast.error('Order successfully created! 📝 Redirecting to payment...', {
+        icon: '✅',
+      });
+
+      if (url) {
+        location.href = url;
+      }
+    } catch (err) {
+      console.log(err);
+      setSubmitting(false);
+      toast.error('Failed to create order', {
+        icon: '❌',
+      });
+    }
+  };
+
+  const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
+    const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
+    updateItemQuantity(id, newQuantity);
+  };
+
+  return (
+    <Container className="mt-10">
+      <Title text="Checkout" className="font-extrabold mb-8 text-[36px]" />
+
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex gap-10">
+            {/* Left part */}
+            <div className="flex flex-col gap-10 flex-1 mb-20">
+              <CheckoutCart
+                onClickCountButton={onClickCountButton}
+                removeCartItem={removeCartItem}
+                items={cartItems}
+                loading={loading}
+              />
+
+              <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+
+              <CheckoutAddressForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+            </div>
+
+            {/* Right part */}
+            <div className="w-[450px]">
+              <CheckoutSidebar totalAmount={totalAmount} loading={loading || submitting} />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
+    </Container>
+  );
 }
