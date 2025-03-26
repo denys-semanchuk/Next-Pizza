bf00f0d14edee689cab1a1050f75256402a17d1e
denys-semanchuk <semanchukdenys@icloud.com>
2025-03-26 21:44:09 +0100

    resolved next config error

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['media.dodostatic.net'],
+    domains: ["media.dodostatic.net"],
   },
-}
+  transpilePackages: ['bcrypt'],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      tls: false,
+      fs: false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+    };
+    // Add this to ignore these modules in the browser
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'bcrypt': false,
+      'https-proxy-agent': false,
+      '@mapbox/node-pre-gyp': false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
